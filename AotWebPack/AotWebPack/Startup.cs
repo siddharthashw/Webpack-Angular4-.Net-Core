9b1a53e6f11dfc2bdb4794c2eaa7c842af0eba86
siddhartha.cb <siddhartha.cb@outlook.com>
2018-07-28 16:36:32 +0530

    test-2

--- AotWebPack/AotWebPack/Startup.cs
+++ AotWebPack/AotWebPack/Startup.cs
@@ -33,6 +33,8 @@ namespace AotWebPack
             services.AddNodeServices();
             // Add framework services.
             services.AddMvc();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
